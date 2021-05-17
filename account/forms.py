ec953538cb74907ca643a0ceb0c412e575d5a6eb
MrRezoo <rezam578@gmail.com>
2021-05-17 18:00:15 +0430

    custom error_messages

--- account/forms.py
+++ account/forms.py
@@ -1,5 +1,11 @@
 from django import forms
 
+messages = {
+    'required': 'این فیلد اجباری است',
+    'invalid': 'لطفا یک ایمیل معتبر وارد کنید',
+
+}
+
 
 class UserLoginForm(forms.Form):
     username = forms.CharField(max_length=30, widget=forms.TextInput(
@@ -10,11 +16,11 @@ class UserLoginForm(forms.Form):
 
 
 class UserRegistrationForm(forms.Form):
-    username = forms.CharField(max_length=30, widget=forms.TextInput(
+    username = forms.CharField(error_messages=messages, max_length=30, widget=forms.TextInput(
         attrs={'class': 'form-control', 'placeholder': 'username', 'type': 'username'}))
 
-    email = forms.EmailField(max_length=50, widget=forms.EmailInput(
+    email = forms.EmailField(error_messages=messages, max_length=50, widget=forms.EmailInput(
         attrs={'class': 'form-control', 'placeholder': 'email', 'type': 'email'}))
 
-    password = forms.CharField(max_length=40, widget=forms.PasswordInput(
+    password = forms.CharField(error_messages=messages, max_length=40, widget=forms.PasswordInput(
         attrs={'class': 'form-control', 'placeholder': 'password', 'type': 'password'}))
