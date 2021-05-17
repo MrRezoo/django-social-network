ab7905e5bead0ca17beb24a2906c47ca96a653fa
MrRezoo <rezam578@gmail.com>
2021-05-17 13:35:54 +0430

    build post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth.models import User
 from django.db import models
 
+
 # Create your models here.
+class Post(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.TextField(max_length=500)
+    slug = models.SlugField(max_length=200)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f'{self.user} - {self.body[:30]}'
